5bacdc6316ca0690c090c2487115eed6253733ee
David Yao <davidyao2011@hotmail.com>
2020-01-11 20:33:09 +1300

    added new post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Davidyao-Blog`,
+    author: `David Yao`,
+    description: `David Yao blog`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `davidyao2011`,
     },
   },
   plugins: [
